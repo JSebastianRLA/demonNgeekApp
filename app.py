08265ea870c949333d96f96f48867cd62b448242
JSebastianRLA <joan.rojas@ngeek.co>
2024-02-06 12:23:15 -0500

    second commit

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Lista de nombres de imágenes para pasar a la plantilla HTML
-    imagenes = ['imagen1.jpg', 'imagen2.jpg']  # Agrega más imágenes si es necesario
+    imagenes = [
+        'f5-logo-rgb.png', 
+        'isos ngeek-09.png',
+        'isos ngeek-10.png' ]  # Agrega más imágenes si es necesario
     return render_template('index.html', imagenes=imagenes)
 
 if __name__ == '__main__':
