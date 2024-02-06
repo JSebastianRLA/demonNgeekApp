369d033506a92de2758360440c3d78ce3f522750
JSebastianRLA <joan.rojas@ngeek.co>
2024-02-06 13:00:25 -0500

    cambios 2

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     # Lista de nombres de imágenes para pasar a la plantilla HTML
     imagenes = [
-        'f5-logo-rgb.png', 
-        'isos ngeek-09.png',
-        'isos ngeek-10.png' ]  # Agrega más imágenes si es necesario
+        'f5-logo.png', 
+        'ngeekImagotipo.png',
+        'ngeekTipografiWithe.png' ]  # Agrega más imágenes si es necesario
     return render_template('index.html', imagenes=imagenes)
 
 if __name__ == '__main__':
