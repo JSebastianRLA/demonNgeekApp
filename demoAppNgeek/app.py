09f244c3dbd0fe6c55c98653a41a41024bc95e43
IngenieroSebastian <macbookproa2251@MacBook-Pro-de-Joan.local>
2024-02-16 13:15:38 -0500

    espero cambios finales

--- demoAppNgeek/app.py
+++ demoAppNgeek/app.py
@@ -10,11 +10,18 @@ def index():
     ip_address = request.remote_addr
     
     # Obtiene el nombre del host del servidor
-    hostname = socket.gethostname()
+    # Intenta resolver inversamente la dirección IP para obtener el nombre de host
+    try:
+        hostname = socket.gethostbyaddr(ip_address)[0]
+    except socket.herror:
+        hostname = "No se pudo obtener el nombre de host"
+    # Obtener headers
+    
+    headerssss = request.headers
     
     # Renderiza la plantilla HTML y pasa la dirección IP como variable
-    return render_template('index.html', ip_address=ip_address, hostname=hostname)
+    return render_template('index.html', ip_address=ip_address, hostname=hostname, headerssss=headerssss)
 
 if __name__ == '__main__':
-      # app.run(host='127.0.0.1', port=5000)
-    app.run(host='192.168.0.168', port=5000)
+      app.run(host='127.0.0.1', port=5000)
+    # app.run(host='192.168.0.168', port=5000)
